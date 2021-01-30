f2970dde8da4439f6cf3a33009afa06e9da3dc79
Aaron <aaron.tdr12@gmail.com>
2021-01-29 23:52:37 -0600

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/veterinaria", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 app.listen(4000, () => {
   console.log("server running ");
