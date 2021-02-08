143678d468602d9f7c89a50bb1f7b79aed650f67
Aaron <aaron.tdr12@gmail.com>
2021-02-07 22:38:25 -0600

    Enable and create the configuration for cors in API/index.js

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
+
+//configure cors
+const whiteList = ["http://localhost:3000"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    const exists = whiteList.some((domain) => domain === origin);
+    if (exists) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by cors"));
+    }
+  },
+};
+
+//Enable cors
+app.use(cors());
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/reminders", {
